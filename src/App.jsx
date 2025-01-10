80c4324356d5d391c0de23526b3155d705728054
Hadyu <hadyuhakim@gmail.com>
2025-01-10 08:34:41 +0700

    create to-do list

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import Todo from "./Components/Todo";
+
+const App = () => {
+  return (
+    <div className="bg-violet-900 grid py-4 min-h-screen">
+      <Todo />
+    </div>
+  );
+};
+
+export default App;
